8ed8e9bacf2b913e2bc978e9a882f8ed8c6dbccc
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-04 15:09:21 -0300

    componente de paginacao

--- src/pages/app/oders/orders.tsx
+++ src/pages/app/oders/orders.tsx
@@ -7,6 +7,7 @@ import {
 
 import { OrderTableFilters } from "@/pages/app/oders/order-table-filters";
 import { OrderTableRow } from "@/pages/app/oders/order-table-row";
+import { Pagination } from "../../../components/pagination";
 
 export function Orders() {
   return (
@@ -14,31 +15,31 @@ export function Orders() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
 
-        <div></div>
-      </div>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableHeader>
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableHead className="w-[64px]"></TableHead>
-              <TableHead className="w-[140px]">Identificador</TableHead>
-              <TableHead className="w-[180px]">Realizado há</TableHead>
-              <TableHead className="w-[140px]">Status</TableHead>
-              <TableHead>Cliente</TableHead>
-              <TableHead className="w-[140px]">Total do pedido</TableHead>
-              <TableHead className="w-[164px]"></TableHead>
-              <TableHead className="w-[132px]"></TableHead>
-            </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => (
+                  <OrderTableRow key={i} />
+                ))}
+              </TableBody>
+            </Table>
+          </div>
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => (
-                <OrderTableRow key={i} />
-              ))}
-            </TableBody>
-          </Table>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
