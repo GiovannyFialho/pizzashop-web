36ecdeace9ebb3288d532ee21892379ec699792e
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-04 16:27:24 -0300

    criacao de pagina 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router";
 import { AppLayout } from "@/pages/_layouts/app";
 import { AuthLayout } from "@/pages/_layouts/auth";
 
+import { NotFound } from "@/pages/404";
 import { Dashboard } from "@/pages/app/dashboard/dashboard";
 import { Orders } from "@/pages/app/oders/orders";
 import { SignIn } from "@/pages/auth/sign-in";
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
