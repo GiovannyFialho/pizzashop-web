28b1caafc79b922fb76ca50acedf97a20b0e69e5
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-04 15:43:42 -0300

    criacao do dashboard - cards informativos

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from "react-router";
 import { AppLayout } from "@/pages/_layouts/app";
 import { AuthLayout } from "@/pages/_layouts/auth";
 
-import { Dashboard } from "@/pages/app/dashboard";
+import { Dashboard } from "@/pages/app/dashboard/dashboard";
 import { Orders } from "@/pages/app/oders/orders";
 import { SignIn } from "@/pages/auth/sign-in";
 import { SignUp } from "@/pages/auth/sign-up";
