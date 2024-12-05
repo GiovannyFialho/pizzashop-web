48a41750a7497905526a1be7e1b7ca9d3f2f215a
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-05 16:05:18 -0300

    instalando libs necessarias para requisicoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { queryClient } from "@/lib/react-query";
 import { router } from "@/routes";
 
 import "@/index.css";
@@ -10,7 +12,9 @@ export function App() {
   return (
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
