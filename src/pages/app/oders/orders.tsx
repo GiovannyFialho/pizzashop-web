ad63c20d98d5b79313e47759799ded4fab9078fd
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-06 14:37:31 -0300

    ajustando paginacao

--- src/pages/app/oders/orders.tsx
+++ src/pages/app/oders/orders.tsx
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+import { z } from "zod";
 
 import { getOrders } from "@/api/get-orders";
 
@@ -15,10 +17,25 @@ import { OrderTableFilters } from "@/pages/app/oders/order-table-filters";
 import { OrderTableRow } from "@/pages/app/oders/order-table-row";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
   const { data: result } = useQuery({
-    queryKey: ["orders"],
-    queryFn: getOrders,
+    queryKey: ["orders", pageIndex],
+    queryFn: () => getOrders({ pageIndex }),
   });
+
+  function handlePaginate(pageIndex: number) {
+    setSearchParams((state) => {
+      state.set("page", (pageIndex + 1).toString());
+
+      return state;
+    });
+  }
 
   return (
     <>
@@ -50,7 +67,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+              onPageChange={handlePaginate}
+            />
+          )}
         </div>
       </div>
     </>
