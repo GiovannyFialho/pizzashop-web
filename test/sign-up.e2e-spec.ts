eeba974e83e3883247522da0ae683f6f783ca427
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2024-12-10 16:26:46 -0300

    criacao de testes para atualizacao de perfil

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -18,7 +18,7 @@ test("sign up successfully", async ({ page }) => {
 test("sign up with error", async ({ page }) => {
   await page.goto("/sign-up", { waitUntil: "networkidle" });
 
-  await page.getByLabel("Nome do estabelecimento").fill("Teste");
+  await page.getByLabel("Nome do estabelecimento").fill("Invalid name");
   await page.getByLabel("Seu nome").fill("John Doe");
   await page.getByLabel("Seu e-mail").fill("johndoe@example.com");
   await page.getByLabel("Seu celular").fill("123812641264");
---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { expect, test } from "@playwright/test";
+
+test("update profile successfully", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  await page.getByRole("button", { name: "Pizza Shop" }).click();
+  await page.getByRole("menuitem", { name: "Perfil da loja" }).click();
+
+  await page.getByLabel("Nome").fill("Giovanny's Pizza");
+  await page.getByLabel("Descrição").fill("Descrição muito bacana!");
+
+  await page.getByRole("button", { name: "Salvar" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  const toast = page.getByText("Perfil atualizado com sucesso!");
+
+  expect(toast).toBeVisible();
+
+  await page.getByRole("button", { name: "Close" }).click();
+
+  expect(page.getByRole("button", { name: "Giovanny's Pizza" })).toBeVisible();
+});
+
+test("update profile wrong", async ({ page }) => {
+  await page.goto("/", { waitUntil: "networkidle" });
+
+  await page.getByRole("button", { name: "Pizza Shop" }).click();
+  await page.getByRole("menuitem", { name: "Perfil da loja" }).click();
+
+  await page.getByLabel("Nome").fill("Invalid name");
+  await page.getByLabel("Descrição").fill("Descrição muito bacana!");
+
+  await page.getByRole("button", { name: "Salvar" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  const toast = page.getByText("Falha ao atualizar o perfil, tente novamente");
+
+  expect(toast).toBeVisible();
+});
